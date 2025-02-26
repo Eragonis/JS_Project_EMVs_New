f83f6fffe2765532692df2caeba7873453406f9c
Monster Musume <patrick.imhof06@gmail.com>
2025-02-26 11:24:23 +0100

    Logic gefixed

--- JavaScript/logic.js
+++ JavaScript/logic.js
@@ -1,13 +1,13 @@
 const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = [0,1,2,3,4,5,6,7,8,9];
 const gridsymbol = "🔴";
+let back = [""]
 let grid = 
 [
     "","","",
     "","","",
     "","",""
 ]
-let back = ["","",""]
 let back_gridOne =
 [
 "","","",
@@ -46,62 +46,54 @@ document.getElementById('mode').addEventListener('change', function(){
     intervalId = setInterval(function () {
         
         if (pattern === 1){
+            
+            if(back.length == 3){
+                back.pop();
+            }
+            
             let random = numbers[Math.floor(Math.random()*numbers.length)]
-            test.innerHTML = random
-            back[increment] = random;
-            increment++;
-            console.log(back);
-           
-            if(increment === 3){
-                increment = 0;
-            }
-        }
-
-        if(pattern === 2){
-            let random = letters[Math.floor(Math.random() * letters.length)];
             test.innerHTML = random;
-            back[increment] = random;
-            increment++; 
-            console.log(back); 
+            back.unshift(random);
+            console.log(back);     
+             
+        };
+                   
+        if(pattern === 2){
+             
+            if(back.length == 3){
+                back.pop();
+            }
+            
+            let random = letters[Math.floor(Math.random()*numbers.length)]
+            test.innerHTML = random;
+            back.unshift(random);
+            console.log(back);
+            
+        }
+            
+        if(pattern === 3){
+            
+           if(grid.length == 3){
+               back.pop();
+           }
            
-            if (increment === 3) {
-                increment = 0; 
-            }
-    
-         }
-         if(pattern === 3){
-            let random = Math.floor(Math.random()*grid.length)
-            grid =
-            [                
-             "","","",
-             "","","",
-             "","",""
-            ]
-            grid[random] = gridsymbol;
+           let random = numbers[Math.floor(Math.random()*numbers.length)]
+           test.innerHTML = random;
+           back.unshift(random);
+           console.log(back);
+           
+        }      
         
-            if(increment === 0){
-                back_gridOne = grid;
-                test.innerHTML = back_gridOne;
-            }
+   
             
-            if(increment === 1){
-                back_gridTwo = grid;               
-                test.innerHTML = back_gridTwo;
-            }
-            if(increment === 2){
-                back_gridThree = grid;       
-                test.innerHTML = back_gridThree            
-            }
-            
-            if(increment != 3){
-               increment++;                
-            }
-            
-            if(increment === 3){
-                increment = 0;        
-            }
+
+        
+        
+        
+        
+        
+         
     
-         }      
        
         
        
